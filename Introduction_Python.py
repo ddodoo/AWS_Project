23535c105d5b8cbe0c368ecd9e0482c5dd1f9f1b
ddodoo <dodoo_daniel@yahoo.co.uk>
2020-11-19 10:12:52 +0000

    AWS_PYTHON

---
+++ Introduction_Python.py
@@ -0,0 +1,24 @@
+print('Daniel is learning python')
+print('I am new with AWS')
+
+x =10
+y = "It's almost time"
+z = True
+a = 10.5
+
+print(type(x))
+print(type(y))
+print(type(z))
+print (type(a))
+
+firstnumber = 50
+secondnumber = 100
+
+total = firstnumber + secondnumber
+
+print(total)
+
+#Multiplication
+
+#num1 = 20
+#num2 = 50
---
+++ strings.py
@@ -0,0 +1,13 @@
+# string
+
+school = "University of Lagos"
+print(school[:10])
+print(school[-3:])
+
+print(len(school))
+print(school.upper())
+print(school.lower())
+print(school.title())
+print(school[0:4].upper())
+print(school[-1].upper())
+print(school.replace('Lagos','Ghanacd'))
