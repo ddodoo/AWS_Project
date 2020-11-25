398ac40731fed8228935654775590fbb8d2dfe9a
ddodoo <dodoo_daniel@yahoo.co.uk>
2020-11-25 19:35:33 +0000

    old_Loop

---
+++ loops_old.py
@@ -0,0 +1,101 @@
+#AWS_Restart
+#Ghanaian Names Days Of The Week
+#Daniel Nii Amu Dodoo
+
+daysOfWeek = []
+
+day_1 = input("Enter the first day of the week:")
+daysOfWeek.append(day_1.capitalize())
+print(daysOfWeek)
+
+day_2 = input("Enter the second day of the week:")
+if  day_1 == day_2:
+    day_2 = input("Enter the second day of the week again!:")
+daysOfWeek.append(day_2.capitalize())
+print(daysOfWeek)
+
+day_3 = input("Enter the third day of the week:")
+if  day_3 == daysOfWeek:
+    day_3 = input("Enter the second day of the week again!:")
+daysOfWeek.append(day_3.capitalize())
+print(daysOfWeek)
+
+day_4 = input("Enter the fourth day of the week:")
+if  day_4 == daysOfWeek:
+    day_4 = input("Enter the fourth of the week again!:")
+daysOfWeek.append(day_4.capitalize())
+print(daysOfWeek)
+
+day_5 = input("Enter the fifth day of the week:")
+if day_5 == daysOfWeek:
+    day_5 = input("Enter the fifth day of the week again!:")
+daysOfWeek.append(day_5.capitalize())
+print(daysOfWeek)
+
+day_6 = input("Enter the sixth day of the week:")
+if day_6 == daysOfWeek:
+    day_6 = input("Enter the sixth day of the week again!:")
+daysOfWeek.append(day_6.capitalize())
+print(daysOfWeek)
+
+day_7 = input("Enter the seventh day of the week:")
+if day_7 == daysOfWeek:
+    day_7 = input("Enter the seventh day of the week again!:")
+daysOfWeek.append(day_7.capitalize())
+print(daysOfWeek)
+
+sex_Male_Female = input("Enter Your Sex, Male or Female:")
+day_Born = input("Enter the day of the week you were born:")
+
+if sex_Male_Female == "Male" and day_Born == "Monday":
+        print("Kwadwo")
+elif sex_Male_Female == "Female" and day_Born == "Monday":
+        print("Ajua")
+
+if sex_Male_Female == "Male" and day_Born == "Tuesday":
+    print("Kwabena")
+
+elif sex_Male_Female == "Female" and day_Born == "Tuesday":
+    print("Abena")
+
+if sex_Male_Female == "Male" and day_Born == "Wednesday":
+    print("Kwaku")
+
+elif sex_Male_Female == "Female" and day_Born == "Wednesday":
+    print("Akua")
+
+if sex_Male_Female == "Male" and day_Born == "Thursday":
+    print("Yaw")
+
+elif sex_Male_Female == "Female" and day_Born == "Thursday":
+    print("Yaa")
+
+if sex_Male_Female == "Male" and day_Born == "Friday":
+    print("Kofi")
+
+elif sex_Male_Female == "Female" and day_Born == "Friday":
+    print("Afia")
+
+if sex_Male_Female == "Male" and day_Born == "Saturday":
+    print("Kwame")
+
+elif sex_Male_Female == "Female" and day_Born == "Saturday":
+    print("Ama")
+
+if sex_Male_Female == "Male" and day_Born == "Sunday":
+    print("Kwesi")
+
+elif sex_Male_Female == "Female" and day_Born == "Sunday":
+    print("Esi")
+
+
+
+
+
+
+
+
+
+
+
+
